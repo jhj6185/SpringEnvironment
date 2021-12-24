37cfd1b77daa66767569ff290ff9b5bfca0426c9
hyeonju9705 <tkaqhcjstk@likelion.org>
2021-12-24 18:00:06 +0900

    스프링 개발환경 jdbc까지완료

---
+++ src/main/java/org/conan/sample/Chef.java
@@ -0,0 +1,11 @@
+package org.conan.sample;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data //getter,setter, toString 다 만들어주는 어노테이션
+public class Chef {
+	
+}
