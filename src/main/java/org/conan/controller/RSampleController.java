7d0c9deccf698540abf6e48216ac71364d73f8c0
hyeonju9705 <tkaqhcjstk@likelion.org>
2022-01-03 09:56:51 +0900

    rest 실습

--- src/main/java/org/conan/controller/RSampleController.java
+++ src/main/java/org/conan/controller/RSampleController.java
@@ -1,11 +1,15 @@
 package org.conan.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import org.conan.domain.SampleVO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,5 +39,24 @@ public class RSampleController {
 	public List<SampleVO> getList(){
 		return IntStream.range(1,10).mapToObj(i -> new SampleVO(i,i+"First",i+"Last"))
 				.collect(Collectors.toList());
+	}
+	
+	@GetMapping(value="/getMap")
+	public Map<String, SampleVO> getMap(){
+		Map<String, SampleVO> map= new HashMap<>();
+		map.put("First", new SampleVO(111, "그루트", "주니어"));
+		return map;
+	}
+	
+	@GetMapping(value="/check", params= {"height", "weight"})
+	public ResponseEntity<SampleVO> check(Double height, Double weight){
+		SampleVO vo = new SampleVO(000, ""+height, ""+weight);
+		ResponseEntity<SampleVO> result= null;
+		if(height<150) {
+			result= ResponseEntity.status(HttpStatus.BAD_GATEWAY).body(vo);
+		}else {
+			result= ResponseEntity.status(HttpStatus.OK).body(vo);
+		}
+		return result;
 	}
 }
